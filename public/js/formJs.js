bf2151766a23f1e6c3c508d1d2a566fd9f370dba
rijvan.juneja <rijvan.juneja.2024@gmail.com>
2024-06-04 18:28:23 +0530

    add validation on medication

--- public/js/formJs.js
+++ public/js/formJs.js
@@ -1,15 +1,20 @@
 
 // ================ show and hide Input field Day ================ //
-document.querySelector("#routing").addEventListener('change', (event) => {
+const hideShowInput = () =>{
     const day = document.querySelector("#day");
     const dayLable = document.querySelector("#dayLable");
-    if (event.target.value === 'weekly') {
+    if (document.querySelector("#routing").value === 'weekly') {
         day.style.display = 'block';
         dayLable.innerHTML = 'Day';
     } else {
         dayLable.innerHTML = '';
         day.style.display = 'none';
+        document.querySelector('.dayError').textContent = ``;
     }
+}
+hideShowInput();
+document.querySelector("#routing").addEventListener('change', (event) => {
+    hideShowInput();
 });
 
 // ===== Sumbit data of form(Only one time,Reccurring form) ====== //
@@ -46,10 +51,97 @@ const addMedication = async (formId,formType) => {
 }
 
 document.querySelector("#otoBtn").addEventListener('click', () => {
-    addMedication("otoForm","oto");
+    if(validations('oto')){
+        addMedication("otoForm","oto");
+    }
 });
 
 document.querySelector("#recurringBtn").addEventListener('click', () => {
-    addMedication("recurringForm","recurrence");
+    if(validations('recurring')){
+        addMedication("recurringForm","recurrence");
+    }
 });
 
+
+
+// ================ validations ================ //
+
+const validateImageFile = (file, errorClass) => {
+    let allowImages = ['image/jpg', 'image/jpeg', 'image/png'];
+    let fileSize = 5; // 5 MB
+  
+    const error = document.querySelector(`.${errorClass}`);
+  
+    if (!(allowImages.includes(file.type))) {
+      error.textContent = `only ${allowImages.toString()} are allowed`;
+      return false;
+    } else if (file.size >= (fileSize * 1024 * 1024)) {
+      error.textContent = `maximum image size is ${fileSize}`;
+      return false;
+    } else {
+      error.textContent = ``;
+    }
+  
+    return true;
+  
+}
+ 
+
+const validations = (formType) => {
+
+    let required = [];
+
+    if(formType == 'recurring'){
+        required.push('routing','time','startDate','endDate')
+        if(document.querySelector('#routing').value == 'weekly'){
+            required.push('day');
+        }
+    } else {
+        required.push('otoDate', 'otoTime');
+    }
+  
+  
+    for (const field of required) {
+      let value = document.querySelector(`#${field}`).value.trim();
+      let errorSpan = document.querySelector(`.${field}Error`);
+      let name = field.replace(/([A-Z])/g, ' $1').trim();
+      name = name.charAt(0).toUpperCase() + name.slice(1);
+      errorSpan.textContent = '';
+  
+      if (required.includes(field) && value == '') {
+        errorSpan.textContent = `${name} is Required`;
+      } else {
+        errorSpan.textContent = ``;
+      }
+    }
+
+    if(formType == 'recurring'){
+        const image = document.querySelector(`#rImage`);
+        if(image.files.length == 0){
+            required.push('rImage');
+            document.querySelector(`.rImageError`).textContent = `Image is Required`;
+        } else if(!(validateImageFile(image.files[0],'rImageError'))){
+            required.push('rImage');
+        }
+    } else {
+        const image = document.querySelector(`#otoImage`);
+        if(image.files.length == 0){
+            required.push('otoImage');
+            document.querySelector(`.otoImageError`).textContent = `Image is Required`;
+        } else if(!(validateImageFile(image.files[0],'otoImageError'))){
+            required.push('otoImage');
+        }
+    }
+    
+    
+    for (const field of required) {
+      let errorSpan = document.querySelector(`.${field}Error`);
+      if (errorSpan.textContent != '') {
+        return false;
+      }
+    }
+  
+    return true;
+} 
+
+  
